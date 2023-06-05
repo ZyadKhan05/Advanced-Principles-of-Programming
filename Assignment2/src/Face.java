97e291107becec827ca71544536800e3c288c78f
Zyad Khan <74572665+ZyadKhan05@users.noreply.github.com>
2023-06-04 20:28:11 -0400

    Delete Assignment2 directory

--- Assignment2/src/Face.java
+++
@@ -1,9 +0,0 @@
-// Parent interface with the attributes Eyes, Nose, and Mouth
-public interface Face {
-
-    public abstract void eyes(); // Interface method (no body)
-
-    public abstract void mouth(); // Interface method (no body)
-
-    public abstract void nose(); // Interface method (no body)
-}
